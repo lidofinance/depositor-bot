e64543b7e102298075e776585c26b5fa19ce395d
Raman <roman.s@p2p.org>
2021-08-12 03:04:14 +0300

    Add readme and script refactor

--- README.md
+++ README.md
@@ -1,8 +1,40 @@
-# Depositor bot
+## Depositor bot
+Bot that will deposit 150 wei if Lido contract is active and has less than MIN_BUFFERED_ETHER in buffer.
 
-## Install all dependencies
-- `pipenv install`
-- `npm install -g ganache-cli`
+## How to install
+
+[Ganache CLI](https://github.com/trufflesuite/ganache-cli) is required to run [Brownie](https://github.com/eth-brownie/brownie)
+
+```bash 
+npm install -g ganache-cli
+```
+
+Via pipenv
+```bash
+git clone git@github.com:lidofinance/depositor-bot.git
+cd depositor-bot
+pipenv install
+```
+
+Other
+
+There is only one dependency, just make sure you are using this fork to install Brownie 
+`https://github.com/vshvsh/brownie.git`
 
 ## Run script
+
 To run script type:  `brownie run depositor`
+
+## Defaults
+
+| Constant                      | Amount     | Description |
+| -------------                 | :--------: | :-----|
+| MAX_GAS_PRICE                 | 100 GWEI   | Bot will wait for a lower price |
+| CONTRACT_GAS_LIMIT            | 10 MWEI    | Default transaction gas limit |
+| DEPOSIT_AMOUNT                | 150 WEI    | Each deposit equals |
+| MIN_BUFFERED_ETHER            | 32 * 8 ETH | In contract should be less ether <br>buffered to be able to deposit |
+
+
+### Lido and stETH token
+
+Lido and stETH token address: `0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84`
--- scripts/depositor.py
+++ scripts/depositor.py
@@ -1,57 +1,69 @@
+import logging
 import time
-import warnings
 
-from brownie import accounts, chain, interface, web3
-from brownie.network.contract import BrownieEnvironmentWarning
-from click import secho, prompt, Choice
+from brownie import accounts, chain, web3, interface
+from brownie.network.account import LocalAccount
 
 
-warnings.filterwarnings('ignore', category=BrownieEnvironmentWarning)
+# Sub-units of Ether
+ETHER = 10**18
+GWEI = 10**9
+MWEI = 10**6
 
+# Transaction limits
+MAX_GAS_PRICE = 100 * GWEI
+CONTRACT_GAS_LIMIT = 10 * MWEI
 
-MIN_BUFFERED_ETHER = 32*8*10**18
-MAX_GAS_PRICE = 100 * 10**9
+# Hardcoded prices
+DEPOSIT_AMOUNT = 150
+MIN_BUFFERED_ETHER = 32 * 8 * ETHER
 
-
-def get_account():
-    return accounts.load(prompt('account', type=Choice(accounts.load())))
-
-
-def get_lido(user=None):
-    return interface.Lido('0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84', owner=user)
+# LIDO
+LIDO_CONTRACT_ADDRESS = '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'
 
 
 def main():
-    user = get_account()
-    lido = get_lido(user)
+    """Transfer tokens from account to LIDO contract while gas price is low"""
+    account = get_account()
+    contract = get_lido_contract(LIDO_CONTRACT_ADDRESS, account)
 
-    for block in chain.new_blocks():
-        secho(f'>>> {block.number}', dim=True)
+    # Transfer money
+    deposit_to_contract(contract, account)
 
-        gas_price = web3.eth.generate_gas_price()
-        secho(f'Gas price: {gas_price/10**9} gwei')
-        if gas_price > MAX_GAS_PRICE:
+
+def get_account() -> LocalAccount:
+    account_files_list = accounts.load()
+
+    # Somehow choose an account?
+    account_filename = account_files_list[0]
+
+    return accounts.load(account_filename)
+
+
+def get_lido_contract(contract_address: str, owner: LocalAccount) -> interface:
+    return interface.Lido(contract_address, owner=owner)
+
+
+def deposit_to_contract(lido: interface, user: LocalAccount):
+    for _ in chain.new_blocks():
+        current_gas_price = web3.eth.generate_gas_price()
+        if current_gas_price > MAX_GAS_PRICE:
             continue
 
-        is_stopped = lido.isStopped()
-        secho(f'Lido is stopped: {is_stopped}')
         if lido.isStopped():
+            # Maybe break here?
             continue
 
-        buffered_ether = lido.getBufferedEther() / 10**18
-        secho(f'Lido\'s buffered ether: {buffered_ether}')
         if lido.getBufferedEther() < MIN_BUFFERED_ETHER:
             continue
 
         try: 
-            tx = lido.depositBufferedEther(150, {
-                'gas_price': gas_price,
+            lido.depositBufferedEther(DEPOSIT_AMOUNT, {
+                'gas_price': current_gas_price,
                 'from': user,
-                'gas_limit': 10000000,
+                'gas_limit': CONTRACT_GAS_LIMIT,
             })
+        except Exception as exception:
+            logging.error(str(exception))
 
-            secho(str(tx))
-        except Exception as e:
-            secho(str(e))
-        
         time.sleep(120)
