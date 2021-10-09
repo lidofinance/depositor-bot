b6c068a7acc6afef719157fdd4d172dfc03c9530
Vasiliy Shapovalov <vs@p2p.org>
2021-10-09 20:41:11 +0300

    feat: faster caching

--- scripts/collect_bc_deposits.py
+++ scripts/collect_bc_deposits.py
@@ -1,4 +1,5 @@
 import json
+import time
 import os
 from collections import Counter
 from fractions import Fraction
@@ -14,12 +15,18 @@ from hexbytes import HexBytes
 
 from joblib import Memory
 
+#TODO - dump is unsafe! rewrite to safe storage later
+import joblib
+
 
 cachedir = 'deposit_contract_cache'
 mem = Memory(cachedir)
+key_cache_path = os.path.join(cachedir, 'deposit_keys_pickle.dump')
+
+
 
 deposit_contract_deployment_block = 11052984  
-end_block = 11283984
+end_block = web3.eth.block_number
 query_step = 1000
 unreorgable_distance = 100
 dc = interface.DepositContract("0x00000000219ab540356cBB839Cbe05303d7705Fa")
@@ -46,7 +53,7 @@ def peek_deposit_contract_events(from_block, to_block):
 
 peek_deposit_contract_historical_events = mem.cache(peek_deposit_contract_events)
 
-def get_deposit_contract_events(deposit_contract_deployment_block, current_block):
+def collect_deposit_contract_historical_events(deposit_contract_deployment_block, current_block):
     fresh_events = peek_deposit_contract_events(current_block - unreorgable_distance, current_block)
     historical_end = current_block - unreorgable_distance - 1
     historical_events = []
@@ -56,13 +63,66 @@ def get_deposit_contract_events(deposit_contract_deployment_block, current_block
         historical_events += logs
     return historical_events + fresh_events
 
-def build_used_pubkeys_map(deposit_events):
+def deposit_events_to_pubkeys(deposit_events):
     used_pubkeys = set()
     for deposit_event in deposit_events:
         used_pubkeys.add(deposit_event['args']['pubkey'])
     return used_pubkeys
 
+def collect_fresh_pubkeys(from_block, to_block):
+    fresh_events = peek_deposit_contract_events(from_block, to_block)
+    return deposit_events_to_pubkeys(fresh_events)
+
+def collect_cached_pubkeys(from_block, to_block):
+    fresh_events = peek_deposit_contract_historical_events(from_block, to_block)
+    return deposit_events_to_pubkeys(fresh_events)
+
+#chp (1500) = chp(1000) + cfp(500)
+#chp (1,4) = сhp(1,2) + cfp(3,4) 
+
+def collect_historical_pubkeys(from_block, to_block, query_step):
+
+    saved_pubkeys = {'last_block' : 0, 'pubkeys': set()}
+
+    try: 
+        saved_pubkeys = joblib.load(key_cache_path)
+    except:
+        print("No saved pubkey dump")
+
+    collection_start = max(from_block, saved_pubkeys['last_block'] +1)
+
+    historical_events = []
+    for start in trange(collection_start, to_block, query_step):
+        end = min(start + query_step - 1, to_block)
+        collected_pubkeys = collect_cached_pubkeys(start, end)
+        saved_pubkeys['pubkeys'] = saved_pubkeys['pubkeys'].union(collected_pubkeys)
+        saved_pubkeys['last_block'] = end
+
+    joblib.dump(saved_pubkeys, key_cache_path)
+
+    return saved_pubkeys['pubkeys']
+
+
+
+
+def build_used_pubkeys_map(from_block, to_block, unreorgable_distance, query_step):
+    length = to_block - from_block + 1
+    unreorgable_length = length - unreorgable_distance
+    historical_length = unreorgable_length - unreorgable_length % unreorgable_distance
+    historical_period_end = from_block + historical_length - 1
+
+    historical_pubkeys = collect_historical_pubkeys(from_block, to_block, query_step)
+
+    fresh_pubkeys = collect_fresh_pubkeys(historical_period_end + 1, to_block)
+
+    return historical_pubkeys.union(fresh_pubkeys)
+
+
 def main():
-    deposit_events = get_deposit_contract_events(deposit_contract_deployment_block, end_block)
-    used_pubkeys = build_used_pubkeys_map(deposit_events)
-    print(used_pubkeys)
+    tic = time.perf_counter()
+    used_pubkeys = build_used_pubkeys_map(deposit_contract_deployment_block, 
+                                end_block, 
+                                unreorgable_distance,
+                                query_step)
+    toc = time.perf_counter()
+    print(f"Got {len(used_pubkeys)} in {toc - tic:0.4f} seconds")
