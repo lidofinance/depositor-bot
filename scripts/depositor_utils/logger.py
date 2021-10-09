b0e9f5e66d6b7d0a1fea3ef5ec7aa58db835d24b
Raman <roman.s@p2p.org>
2021-10-09 17:03:31 +0300

    add loki and prometheus

--- scripts/depositor_utils/logger.py
+++ scripts/depositor_utils/logger.py
@@ -1,5 +1,7 @@
 import logging
 
+import logging_loki
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,5 +12,12 @@ logging.basicConfig(
     ]
 )
 
+handler = logging_loki.LokiHandler(
+    url="https://logs-prod-us-central1.grafana.net/loki/api/v1/push",
+    tags={"application": "deposit-bot"},
+    auth=("103382", "eyJrIjoiZWZmYWVlYWMyZjYwZmI2YTE0MWMyZDZlMjIxY2M0NDZiYTBmOGZiNiIsIm4iOiJuZXcga2V5IiwiaWQiOjU0NDAxNX0="),
+    version="1",
+)
 
 logger = logging.getLogger("depositor-bot")
+logger.addHandler(handler)
