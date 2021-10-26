7c0238d84e147dc5b7c9873c53ea04c4632c70a4
Raman <roman.s@p2p.org>
2021-10-26 14:41:25 +0300

    new formatted logs

--- scripts/depositor_utils/logger.py
+++ scripts/depositor_utils/logger.py
@@ -1,10 +1,28 @@
+import json
 import logging
+
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        to_json_msg = json.dumps({
+            'name': record.name,
+            'levelname': record.levelname,
+            'funcName': record.funcName,
+            'lineno': record.lineno,
+            'module': record.module,
+            'pathname': record.pathname,
+            **record.msg,
+        })
+        return to_json_msg
+
+
+steam_log_handler = logging.StreamHandler()
+steam_log_handler.setFormatter(JsonFormatter())
 
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    handlers=[logging.StreamHandler()]
+    handlers=[steam_log_handler]
 )
 
 logger = logging.getLogger("depositor-bot")
