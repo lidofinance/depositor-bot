580dacb7b0b35fd9afd5c27da192a891ec6f2f8a
Raman <roman.s@p2p.org>
2022-01-31 11:29:47 +0300

    Flashbot signature is required

--- scripts/utils/variables.py
+++ scripts/utils/variables.py
@@ -36,7 +36,7 @@ WEB3_CHAIN_ID = web3.eth.chain_id
 
 # Account private key
 WALLET_PRIVATE_KEY = os.getenv('WALLET_PRIVATE_KEY', None)
-FLASHBOT_SIGNATURE = os.getenv('FLASHBOT_SIGNATURE', web3.eth.account.create().privateKey.hex())
+FLASHBOT_SIGNATURE = os.getenv('FLASHBOT_SIGNATURE')
 
 if WALLET_PRIVATE_KEY:
     ACCOUNT = accounts.add(WALLET_PRIVATE_KEY)
