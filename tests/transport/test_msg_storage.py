28f4301208511de8dec5438285d7638545504f19
F4ever <1590415904a@gmail.com>
2023-09-12 09:56:40 +0200

    add checksum test

---
+++ tests/transport/test_msg_storage.py
@@ -0,0 +1,32 @@
+import pytest
+
+from transport.msg_schemas import to_check_sum_address
+from transport.msg_storage import MessageStorage
+
+
+class Transport:
+    def get_messages(self):
+        return [
+            {'guardianAddress': '0x5fd0ddbc3351d009eb3f88de7cd081a614c519f1'},
+            {'guardianAddress': '0x3dc4cF780F2599B528F37dedB34449Fb65Ef7d4A'},
+        ]
+
+
+@pytest.fixture
+def msg_storage():
+    yield MessageStorage(
+        [Transport()],
+        filters=[
+            to_check_sum_address,
+        ],
+    )
+
+
+@pytest.mark.unit
+def test_checksum_address_parsing(msg_storage: MessageStorage):
+    updated_msgs = msg_storage.get_messages(lambda x: x)
+
+    assert updated_msgs == [
+        {'guardianAddress': '0x5fd0dDbC3351d009eb3f88DE7Cd081a614C519F1'},
+        {'guardianAddress': '0x3dc4cF780F2599B528F37dedB34449Fb65Ef7d4A'},
+    ]
