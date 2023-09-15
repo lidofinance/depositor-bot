579f9040cace35329a23d3dd3433ca0b48b207b1
F4ever <1590415904a@gmail.com>
2023-09-15 15:45:41 +0200

    add preferred deposit module func

---
+++ src/blockchain/deposit_strategy/prefered_module_to_deposit.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+from blockchain.typings import Web3
+
+
+def get_preferred_to_deposit_module(w3: Web3, whitelist_modules: list[int]) -> Optional[int]:
+    """
+    Returns preferable module to deposit to make deposits balanced following the rules specified in Staking Router.
+    Order is
+    1. What module can accept bigger deposit? Checks available keys amount
+    2. Check which module accepts more staking allocation
+    """
+    active_modules = get_active_modules(w3, whitelist_modules)
+
+    if not active_modules:
+        return None
+
+    stats = get_modules_stats(w3, active_modules)
+
+    # Return module id
+    return stats[0][2]
+
+
+def get_active_modules(w3: Web3, whitelist_modules: list[int]) -> list[int]:
+    # Get all module ids
+    modules = w3.lido.staking_router.get_staking_module_ids()
+
+    # Filter not-whitelisted modules
+    modules = [module for module in modules if module in whitelist_modules]
+
+    # Filter not-active modules
+    modules = [module for module in modules if w3.lido.staking_router.is_staking_module_active(module)]
+
+    return modules
+
+
+def get_modules_stats(w3: Web3, modules: list[int]) -> list[tuple[int, int, int]]:
+    max_deposits_count = w3.lido.deposit_security_module.get_max_deposits()
+    depositable_ether = w3.lido.lido.get_depositable_ether()
+
+    module_stats = [(
+            w3.lido.staking_router.get_staking_module_max_deposits_count(module, depositable_ether),
+            w3.lido.staking_router.get_staking_module_max_deposits_count(module, max_deposits_count * 32 * 10 ** 18),
+            module,
+        ) for module in modules
+    ]
+
+    module_stats = sorted(module_stats, reverse=True)
+
+    return module_stats
---
+++ tests/blockchain/deposit_strategy/test_get_prefered_module_to_deposit.py
@@ -0,0 +1,48 @@
+from unittest.mock import Mock
+
+import pytest
+
+from blockchain.deposit_strategy.prefered_module_to_deposit import (
+    get_preferred_to_deposit_module,
+    get_active_modules,
+    get_modules_stats,
+)
+
+
+@pytest.mark.unit
+def test_get_preferred_to_deposit_module(web3_lido_unit):
+    modules = list(range(10))
+
+    web3_lido_unit.lido.deposit_security_module.get_max_deposits = Mock(return_value=100)
+    web3_lido_unit.lido.lido.get_depositable_ether = Mock(return_value=10 * 32 * 10 ** 18)
+    web3_lido_unit.lido.staking_router.get_staking_module_ids = Mock(return_value=modules)
+    web3_lido_unit.lido.staking_router.get_staking_module_max_deposits_count = Mock(return_value=0)
+
+    result = get_preferred_to_deposit_module(web3_lido_unit, modules[:-2])
+
+    assert result == 7
+
+
+def test_active_modules(web3_lido_unit):
+    web3_lido_unit.lido.staking_router.get_staking_module_ids = Mock(return_value=[1, 2, 3, 4, 5, 6])
+    web3_lido_unit.lido.staking_router.is_staking_module_active = lambda x: x % 2
+
+    modules_list = get_active_modules(web3_lido_unit, [1, 2, 3, 4])
+
+    assert modules_list == [1, 3]
+
+
+def test_get_module_stats(web3_lido_unit):
+    web3_lido_unit.lido.deposit_security_module.get_max_deposits = Mock(return_value=100)
+    web3_lido_unit.lido.lido.get_depositable_ether = Mock(return_value=10 * 32 * 10 ** 18)
+    web3_lido_unit.lido.staking_router.get_staking_module_max_deposits_count = lambda x, y: x % 3
+
+    stats = get_modules_stats(web3_lido_unit, modules=list(range(8)))
+
+    for i in range(len(stats) - 1):
+        assert stats[i][0] >= stats[i + 1][0]
+
+        if stats[i][1] > stats[i + 1][1]:
+            assert stats[i][2] <= stats[i - 1][2]
+        elif stats[i][1] == stats[i + 1][1]:
+            assert stats[i][0] == stats[i + 1][0]
