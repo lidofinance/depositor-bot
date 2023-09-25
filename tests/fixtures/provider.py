568ab936c3db5efb83a9e49f3b1e02ebe67df0d4
Raman Siamionau <raman.s@lido.fi>
2023-09-25 15:30:13 +0200

    add chronix

--- tests/fixtures/provider.py
+++ tests/fixtures/provider.py
@@ -1,6 +1,7 @@
 from unittest.mock import Mock
 
 import pytest
+import requests
 from web3 import Web3
 from web3_multi_provider import FallbackProvider
 
@@ -17,14 +18,36 @@ def web3_lido_unit():
     web3.attach_modules({
         'transaction': TransactionUtils,
     })
-    
+
     yield web3
 
 
 # -- Integration fixtures --
 @pytest.fixture
 def web3_provider_integration():
-    yield Web3(FallbackProvider(variables.WEB3_RPC_ENDPOINTS))
+    chronix_url = 'http://0.0.0.0:8080/'
+    hardhat_path = 'v1/env/hardhat/'
+
+    response = requests.put(
+        chronix_url + hardhat_path,
+        json={
+            'chainId': 1,
+            'fork': variables.WEB3_RPC_ENDPOINTS[0],
+            'mining': {
+                'auto': True,
+                'interval': 12000
+            }
+        },
+        headers={
+            'Content-Type': 'application/json'
+        },
+    )
+
+    port = response.json()['data']['port']
+
+    yield Web3(FallbackProvider([f'http://0.0.0.0:{port}/']))
+
+    requests.delete(chronix_url + hardhat_path, json={'port': port})
 
 
 @pytest.fixture
