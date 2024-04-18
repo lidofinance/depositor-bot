f4340ed20a307404b68dac58bde61c7e5342566e
F4ever <raman.s@lido.fi>
2024-04-18 11:44:15 +0200

    update bytecode

--- src/blockchain/web3_extentions/lido_contracts.py
+++ src/blockchain/web3_extentions/lido_contracts.py
@@ -1,8 +1,9 @@
-import os
+import logging
 from typing import cast
 
 from web3 import Web3
 from web3.contract import Contract
+from web3.exceptions import ContractLogicError
 from web3.module import Module
 
 import variables
@@ -11,6 +12,9 @@ from blockchain.contracts.deposit_security_module import DepositSecurityModuleCo
 from blockchain.contracts.lido import LidoContract
 from blockchain.contracts.lido_locator import LidoLocatorContract
 from blockchain.contracts.staking_router import StakingRouterContract
+
+
+logger = logging.getLogger(__name__)
 
 
 class LidoContracts(Module):
@@ -46,31 +50,20 @@ class LidoContracts(Module):
             ContractFactoryClass=StakingRouterContract,
         ))
 
-        # Since we don't have any version in contracts, addresses is the only chance to correctly handle upgrade
-        dsm_v = os.getenv('DEPOSIT_SECURITY_MODULE_VERSION', None)
         dsm_address = self.lido_locator.deposit_security_module()
-        if dsm_v == '1':
-            self.deposit_security_module = cast(DepositSecurityModuleContract, self.w3.eth.contract(
-                address=dsm_address,
-                ContractFactoryClass=DepositSecurityModuleContract,
-            ))
-        elif dsm_v == '2':
-            self.deposit_security_module = cast(DepositSecurityModuleContractV2, self.w3.eth.contract(
-                address=dsm_address,
-                ContractFactoryClass=DepositSecurityModuleContractV2,
-            ))
-        elif dsm_address in [
-            '0xC77F8768774E1c9244BEed705C4354f2113CFc09',  # mainnet
-            '0xe57025E250275cA56f92d76660DEcfc490C7E79A',  # goerli
-            '0x045dd46212A178428c088573A7d102B9d89a022A',  # holesky
-            '0x6885E36BFcb68CB383DfE90023a462C03BCB2AE5',  # sepolia
-        ]:
+
+        self.deposit_security_module = cast(DepositSecurityModuleContractV2, self.w3.eth.contract(
+            address=dsm_address,
+            ContractFactoryClass=DepositSecurityModuleContractV2,
+        ))
+
+        try:
+            self.deposit_security_module.functions.VERSION().call()
+        except ContractLogicError:
+            logger.info({'msg': 'Use deposit security module V1.'})
             self.deposit_security_module = cast(DepositSecurityModuleContract, self.w3.eth.contract(
                 address=dsm_address,
                 ContractFactoryClass=DepositSecurityModuleContract,
             ))
         else:
-            self.deposit_security_module = cast(DepositSecurityModuleContractV2, self.w3.eth.contract(
-                address=dsm_address,
-                ContractFactoryClass=DepositSecurityModuleContractV2,
-            ))
+            logger.info({'msg': 'Use deposit security module V2.'})
