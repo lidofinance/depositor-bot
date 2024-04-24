10f87eb092de7a6e0a761d2fdb16db1de637c461
F4ever <raman.s@lido.fi>
2024-04-24 15:23:08 +0200

    fix tests

--- tests/blockchain/deposit_strategy/test_get_prefered_module_to_deposit.py
+++ tests/blockchain/deposit_strategy/test_get_prefered_module_to_deposit.py
@@ -2,70 +2,57 @@ from unittest.mock import Mock
 
 import pytest
 
-from blockchain.deposit_strategy.prefered_module_to_deposit import get_preferred_to_deposit_modules
+from blockchain.deposit_strategy.prefered_module_to_deposit import (
+    get_preferred_to_deposit_modules,
+    get_module_depositable_filter,
+    prioritize_modules,
+)
 
 
 @pytest.mark.unit
-@pytest.mark.xfail
-def test_get_preferred_to_deposit_modules(web3_lido_unit):
-    """
-    ToDo fixme
-    """
-    modules = list(range(10))
+def test_get_preferred_to_deposit_module(web3_lido_unit):
+    web3_lido_unit.lido.staking_router.get_staking_module_ids = Mock(return_value=[1,2,3,4])
+    web3_lido_unit.lido.staking_router.get_staking_module_digests = Mock(return_value=[
+        (0, 0, (1,), (10, 20, 10)),
+        (0, 0, (2,), (0, 10, 10,)),
+        (0, 0, (3,), (3000, 4000, 10,)),
+        (0, 0, (6,), (5, 10, 10,)),
+        (0, 0, (12,), (3000, 4000, 10,)),
+    ])
+    web3_lido_unit.lido.staking_router.is_staking_module_active = lambda x: not x % 2
+    web3_lido_unit.lido.deposit_security_module.can_deposit = lambda x: not x % 2
 
-    web3_lido_unit.lido.lido.get_depositable_ether = Mock(return_value=10 * 32 * 10 ** 18)
-    web3_lido_unit.lido.staking_router.get_staking_module_ids = Mock(return_value=modules)
-    web3_lido_unit.lido.staking_router.get_staking_module_max_deposits_count = Mock(return_value=0)
-    web3_lido_unit.lido.deposit_security_module.get_max_deposits = Mock(return_value=100)
+    result = get_preferred_to_deposit_modules(web3_lido_unit, [1,2,3,6])
 
-    result = get_preferred_to_deposit_modules(web3_lido_unit, modules[:-2])
-
-    assert result == 7
-
-
-@pytest.mark.unit
-@pytest.mark.xfail
-def test_active_modules(web3_lido_unit):
-    """
-    ToDo fixme
-    """
-    web3_lido_unit.lido.staking_router.get_staking_module_ids = Mock(return_value=[1, 2, 3, 4, 5, 6])
-    web3_lido_unit.lido.staking_router.is_staking_module_active = lambda x: x % 2
-
-    modules_list = get_active_modules(web3_lido_unit, [1, 2, 3, 4])
-
-    assert modules_list == [1, 3]
+    assert result == [6, 2]
 
 
 @pytest.mark.unit
-@pytest.mark.xfail
-def test_non_active_module(web3_lido_unit):
-    """
-    ToDo fixme
-    """
-    web3_lido_unit.lido.staking_router.get_staking_module_ids = Mock(return_value=[1, 2, 3, 4, 5, 6])
-    web3_lido_unit.lido.staking_router.is_staking_module_active = lambda x: x % 4
-    web3_lido_unit.lido.deposit_security_module.can_deposit = lambda x: x % 3
+def test_prioritize_modules(web3_lido_unit):
+    order = prioritize_modules([
+        (0, 0, (0,), (10, 20, 10)),
+        (0, 0, (1,), (5, 10, 10,)),
+        (0, 0, (2,), (3000, 4000, 10,)),
+    ])
 
-    modules_list = get_active_modules(web3_lido_unit, [1, 2, 3, 4])
-
-    assert modules_list == [1, 2]
+    assert order == [1, 0, 2]
 
 
 @pytest.mark.unit
-@pytest.mark.xfail
-def test_get_module_stats(web3_lido_unit):
-    """
-    ToDo fixme
-    """
-    web3_lido_unit.lido.lido.get_depositable_ether = Mock(return_value=10 * 32 * 10 ** 18)
-    web3_lido_unit.lido.staking_router.get_staking_module_max_deposits_count = lambda x, y: x % 3
-    web3_lido_unit.lido.deposit_security_module.get_max_deposits = Mock(return_value=100)
+def test_get_module_filter(web3_lido_unit):
+    web3_lido_unit.lido.staking_router.is_staking_module_active = lambda x: not x % 2
+    web3_lido_unit.lido.deposit_security_module.can_deposit = lambda x: not x % 3
 
-    stats = get_modules_stats(web3_lido_unit, modules=list(range(8)))
+    module_filter = get_module_depositable_filter(web3_lido_unit, [0,1,2,3,4,5,6])
+    available_modules = list(filter(module_filter, [
+        (0, 0, (0,), (0,)),
+        (0, 0, (1,), (0,)),
+        (0, 0, (2,), (0,)),
+        (0, 0, (3,), (0,)),
+        (0, 0, (4,), (0,)),
+        (0, 0, (5,), (0,)),
+        (0, 0, (6,), (0,)),
+        (0, 0, (12,), (0,)),
+    ]))
 
-    for i in range(len(stats) - 1):
-        assert stats[i][0] >= stats[i + 1][0]
-
-        if stats[i][0] == stats[i + 1][0]:
-            assert stats[i + 1][1] < stats[i][1]
+    assert available_modules == [(0, 0, (0,), (0,)), (0, 0, (6,), (0,))]
