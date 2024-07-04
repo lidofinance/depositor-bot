b14b7e0cef18e03c11e31ed746b794b02dcebd01
hweawer <alexandergusakovsky@gmail.com>
2024-07-04 11:51:49 +0200

    Fix Info metircs endpoint(#231)
    * Fix log string
    
    * Change forematting
    
    * Convert values to strings
    
    * remove convertion

--- src/main.py
+++ src/main.py
@@ -25,7 +25,13 @@ class BotModule(StrEnum):
 
 
 def main(bot_name: str):
-    logger.info({'msg': 'Depositor bot env variables', 'value': variables.PUBLIC_ENV_VARS})
+    logger.info(
+        {
+            'msg': 'Bot env variables',
+            'value': variables.PUBLIC_ENV_VARS,
+            'bot_name': bot_name
+        }
+    )
     if bot_name not in list(BotModule):
         msg = f'Last arg should be one of {[str(item) for item in BotModule]}, received {BotModule}.'
         logger.error({'msg': msg})
