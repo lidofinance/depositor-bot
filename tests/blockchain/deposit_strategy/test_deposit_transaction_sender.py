c0c7c75ef69272fb7dba90fea35137acb862e652
hweawer <alexandergusakovsky@gmail.com>
2024-07-18 19:20:46 +0200

    Fix unit test

--- tests/blockchain/deposit_strategy/test_deposit_transaction_sender.py
+++ tests/blockchain/deposit_strategy/test_deposit_transaction_sender.py
@@ -1,14 +1,15 @@
 from unittest.mock import Mock
 
 import pytest
+
 from transport.msg_types.deposit import DepositMessage
 
 MODULE_ID = 1
 
 
 @pytest.mark.unit
-def test_send_deposit_tx(cmds):
-    cmds.w3.transaction.check = Mock(return_value=False)
+def test_send_deposit_tx_not_mellow(deposit_transaction_sender):
+    deposit_transaction_sender._w3.transaction.check = Mock(return_value=False)
     messages = [DepositMessage(
         type='deposit',
         depositRoot='',
@@ -24,13 +25,11 @@ def test_send_deposit_tx(cmds):
         },
         app={'version': '1.0.3', 'name': 'lido-council-daemon'},
     )]
-    cmds._prepare_signs_for_deposit = Mock(return_value=tuple())
-    cmds.is_gas_price_ok = Mock(return_value=True)
-    cmds.deposited_keys_amount = Mock(return_value=True)
-    assert not cmds.prepare_and_send(messages, False)
+    deposit_transaction_sender._prepare_signs_for_deposit = Mock(return_value=tuple())
+    assert not deposit_transaction_sender.prepare_and_send(messages, False, False)
+    assert deposit_transaction_sender._w3.lido.deposit_security_module.deposit_buffered_ether.called
 
-    cmds.w3.transaction.check = Mock(return_value=True)
-    cmds.w3.transaction.send = Mock(return_value=True)
-    assert cmds.prepare_and_send(messages, False)
-    cmds.w3.transaction.send = Mock(return_value=False)
-    assert not cmds.prepare_and_send(messages, False)
+    deposit_transaction_sender._w3.transaction.check = Mock(return_value=True)
+    deposit_transaction_sender._w3.transaction.send = Mock(return_value=True)
+    assert deposit_transaction_sender.prepare_and_send(messages, False, False)
+    assert deposit_transaction_sender._w3.lido.deposit_security_module.deposit_buffered_ether.called
