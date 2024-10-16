285f337a7ab4ec4f45e3bc46e6b328dc46d5fa36
hweawer <alexandergusakovsky@gmail.com>
2024-10-16 18:59:09 +0200

    Remove special forks annotations

--- tests/blockchain/contracts/test_simple_dvt_staking_strategy.py
+++ tests/blockchain/contracts/test_simple_dvt_staking_strategy.py
@@ -4,7 +4,12 @@ from tests.utils.contract_utils import check_contract
 from tests.utils.regrex import ADDRESS_REGREX, check_value_re
 
 
-@pytest.mark.integration_holesky
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    'web3_provider_integration',
+    [{'endpoint': 'https://ethereum-holesky-rpc.publicnode.com'}],
+    indirect=['web3_provider_integration'],
+)
 def test_simple_dvt_staking_strategy_contract_call(simple_dvt_staking_strategy, caplog):
     check_contract(
         simple_dvt_staking_strategy,
--- tests/blockchain/contracts/test_staking_module.py
+++ tests/blockchain/contracts/test_staking_module.py
@@ -4,7 +4,12 @@ from tests.utils.contract_utils import check_contract
 from tests.utils.regrex import ADDRESS_REGREX, check_value_re, check_value_type
 
 
-@pytest.mark.integration_holesky
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    'web3_provider_integration',
+    [{'endpoint': 'https://ethereum-holesky-rpc.publicnode.com'}],
+    indirect=['web3_provider_integration'],
+)
 def test_staking_module_contract_call(staking_module, caplog):
     check_contract(
         staking_module,
