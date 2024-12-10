f8c3d56c62b58b34da8e2db8690f7b23ba6831cf
hweawer <alexandergusakovsky@gmail.com>
2024-12-10 11:03:17 +0100

    Fix recursion (#292)
    * Fix recursion
    
    * Remove metrics from onchain web3

--- src/transport/msg_providers/onchain_transport.py
+++ src/transport/msg_providers/onchain_transport.py
@@ -3,7 +3,7 @@ import logging
 from typing import Callable, List, Optional
 
 import variables
-from blockchain.web3_extentions.middleware import add_cache_middleware, add_requests_metric_middleware
+from blockchain.web3_extentions.middleware import add_cache_middleware
 from eth_typing import ChecksumAddress
 from eth_utils import to_bytes
 from schema import Schema
@@ -326,4 +326,4 @@ class OnchainTransportProvider(BaseMessageProvider):
 
     @staticmethod
     def create_ochain_transport_w3() -> Web3:
-        return add_requests_metric_middleware(add_cache_middleware(Web3(FallbackProvider(variables.ONCHAIN_TRANSPORT_RPC_ENDPOINTS))))
+        return add_cache_middleware(Web3(FallbackProvider(variables.ONCHAIN_TRANSPORT_RPC_ENDPOINTS)))
