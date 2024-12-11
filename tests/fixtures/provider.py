f16d2f60b02e370a62725c2cd134fff08040bb84
hweawer <alexandergusakovsky@gmail.com>
2024-12-11 09:01:54 +0100

    Reorder middlewares (#294)

--- tests/fixtures/provider.py
+++ tests/fixtures/provider.py
@@ -4,6 +4,7 @@ from unittest.mock import Mock
 import pytest
 import variables
 from blockchain.web3_extentions.lido_contracts import LidoContracts
+from blockchain.web3_extentions.middleware import add_middlewares
 from blockchain.web3_extentions.transaction import TransactionUtils
 from web3 import HTTPProvider, Web3
 
@@ -33,9 +34,10 @@ def web3_provider_integration(request) -> Web3:
     anvil_path = os.getenv('ANVIL_PATH', '')
 
     with anvil_fork(anvil_path, rpc_endpoint, block_num):
-        web3 = Web3(HTTPProvider('http://127.0.0.1:8545', request_kwargs={'timeout': 3600}))
-        assert web3.is_connected(), 'Failed to connect to the Web3 provider.'
-        yield web3
+        w3 = Web3(HTTPProvider('http://127.0.0.1:8545', request_kwargs={'timeout': 3600}))
+        add_middlewares(w3)
+        assert w3.is_connected(), 'Failed to connect to the Web3 provider.'
+        yield w3
 
 
 @pytest.fixture
